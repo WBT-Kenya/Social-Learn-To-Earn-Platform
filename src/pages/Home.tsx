ac8bb1053e354a91e2613539fb7d7bc9d97cc693
apella1 <johncollons5@gmail.com>
2023-06-20 14:51:39 +0300

    Update home page navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Sidebar } from "../components";
+import { Navbar, Sidebar } from "../components";
 
 const Home = () => {
   return (
@@ -6,21 +6,27 @@ const Home = () => {
       <section className="basis-1/5">
         <Sidebar />
       </section>
-      <section className="basis-4/5 flex flex-col">
-        <nav className="flex space-x-6 px-16 py-4">
-          <a
-            href="/login"
-            className="text-center w-full text-white bg-[#2D3B79] py-4 px-16 text-[16px] leading-[24px] rounded-[29px]"
-          >
-            Login
-          </a>
-          <a
-            href="/signup"
-            className="text-center w-full text-white bg-[#2D3B79] py-4 px-20 text-[16px] leading-[24px] rounded-[29px]"
-          >
-            Register
-          </a>
-        </nav>
+      <section className="basis-4/5 flex flex-col space-y-6 pt-8">
+        <div className="flex justify-between items-center">
+          <h3 className="text-5xl text-black font-bold">Home</h3>
+          <Navbar />
+        </div>
+        <section className="basis-4/5 flex flex-col">
+          <nav className="flex space-x-6 px-16 py-4">
+            <a
+              href="/login"
+              className="text-center w-full text-white bg-[#2D3B79] py-4 px-16 text-[16px] leading-[24px] rounded-[29px]"
+            >
+              Login
+            </a>
+            <a
+              href="/signup"
+              className="text-center w-full text-white bg-[#2D3B79] py-4 px-20 text-[16px] leading-[24px] rounded-[29px]"
+            >
+              Register
+            </a>
+          </nav>
+        </section>
       </section>
     </section>
   );
