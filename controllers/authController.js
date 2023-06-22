909eab82ad84d3e62fe20703fd74c06df843d8bb
Harriet <harrietodima@gmail.com>
2023-06-22 14:43:07 +0300

    auto account craetion updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,28 +1,31 @@
-const User = require('../models/User');
+const User = require("../models/User");
 module.exports.signup_get = (req, res) => {
-    res.render('signup');
-
-}
+  res.render("signup");
+};
 module.exports.login_get = (req, res) => {
-    res.render('login');
-}
+  res.render("login");
+};
 module.exports.signup_post = async (req, res) => {
-    const { firstName, lastName,email,phonenumber, password } = req.body
+  const { firstName, lastName, email, phoneNumber, password } = req.body;
 
-    try{
-       const user = await User.create({firstName, lastName, email, phonenumber, password});
-       res.status(201).json(user);
-    }
-    catch (err){
-        console.log(err);
-        res.status(400).send('error, user not created');
-
-    };
-}
+  try {
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      password,
+    });
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("error, user not created");
+  }
+};
 
 module.exports.login_post = (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    console.log(email.password);
-    res.send('user login');
+  console.log(email.password);
+  res.send("user login");
 };
